36093eb89f5d9c976125f070f2d81be4a5b87762
David Kamayor <davidkamayor@gmail.com>
2023-03-12 14:04:54 -0400

    Add grid generation functionality

---
+++ script.js
@@ -0,0 +1,37 @@
+const gridContainer = document.getElementById("grid-container");
+let gridItems = document.querySelectorAll(".grid-item");
+
+function makeGrid(rows, cols) {
+  gridContainer.style.setProperty("--grid-rows", rows);
+  gridContainer.style.setProperty("--grid-cols", cols);
+  for (c = 0; c < rows * cols; c++) {
+    let cell = document.createElement("div");
+    cell.innerText = c + 1;
+    gridContainer.appendChild(cell).className = "grid-item";
+    gridItems = document.querySelectorAll(".grid-item");
+  }
+}
+
+makeGrid(16, 16);
+
+function addHoverListener() {
+  gridItems.forEach((gridItem) => {
+    gridItem.addEventListener("mouseover", () => {
+      gridItem.style.backgroundColor = "black";
+    });
+  });
+}
+
+function replaceGrid() {
+  gridItems = document.querySelectorAll(".grid-item");
+  gridItems.forEach((gridItem) => {
+    gridItem.remove();
+  });
+  const gridDimension = prompt("Enter grid size");
+  if (gridDimension > 0 && gridDimension <= 100) {
+    makeGrid(gridDimension, gridDimension);
+  }
+  addHoverListener();
+}
+
+addHoverListener();
