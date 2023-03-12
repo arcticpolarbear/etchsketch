89e71c8ee1f6a2cacf8facf9dd071aee1371fb90
David Kamayor <davidkamayor@gmail.com>
2023-03-12 14:38:12 -0400

    Remove grid numbers

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ function makeGrid(rows, cols) {
   gridContainer.style.setProperty("--grid-cols", cols);
   for (c = 0; c < rows * cols; c++) {
     let cell = document.createElement("div");
-    cell.innerText = c + 1;
     gridContainer.appendChild(cell).className = "grid-item";
     gridItems = document.querySelectorAll(".grid-item");
   }
